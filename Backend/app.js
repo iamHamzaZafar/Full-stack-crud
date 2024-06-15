e86006eadf92289535b18982d785862a21f12960
Hamza Zafar <link2hamzazafar@gmail.com>
2024-06-15 20:21:43 +0500

    added delete create and find user controlleres

--- Backend/app.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const port = 5000;
-
-const app = express();
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
-
-export default app;
--- Backend/index.js
+++ Backend/index.js
@@ -1,19 +1,87 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import db_connection from './dbConnect.js';
+import db_connection from "./dbConnect.js";
+import express from "express";
+import { User } from "./model/user.model.js";
+const app = express();
 
-const port = 5000 ; 
+const port = process.env.PORT || 5000;
 
+app.use(express.json());
 
+db_connection();
 
+// get all the users data from the database
+app.get("/", async (req, res) => {
+  const data = await User.find({});
+  res.json({ msg: "success", data: data });
+});
 
-db_connection()
-.then(()=>{
-   console.log(`Server Running on Port : ${port}`)
-})
+// create data || save data in the data base
+app.post("/create", async (req, res) => {
+  const { name, email, contact } = req.body;
+  // checks name email and password are given or not provided
+  if (!name || !email || !contact) {
+    return res.status(400).json({ error: "Required field is missing" });
+  }
 
-.catch((err)=>{
-    console.log(err)
-})
+  // Check existing user
+  try {
+    const existing = await User.findOne({ email });
+    if (existing) {
+      return res.status(400).json({ error: "User already exists" });
+    }
 
+    const user = new User({ name, email, contact });
+    const data = await user.save();
+    res
+      .status(201)
+      .json({ message: "User registered successfully", data: data });
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
 
+// Update User data
+
+app.put("/update", async (req, res) => {
+  //   const { id } = req.params;
+  const id = req.body;
+  console.log(id);
+  const { name, email, contact } = req.body;
+  if (!name || !email || !contact) {
+    return res
+      .status(400)
+      .json({ error: "At least one field must be provided for update" });
+  }
+
+  try {
+    const user = user.findById(id);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+  } catch (error) {}
+});
+
+// Delete User controller
+app.delete("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    console.log(user.name, user.email, user.contact);
+    await user.deleteOne();
+
+    res.status(200).json({ success: "User deleted Successfully" });
+  } catch (error) {
+    res.status(500).json({ msg: "Server Error" });
+  }
+});
+
+// Server listening
+app.listen(port, () => {
+  console.log("Server listening on port : ", port);
+});
